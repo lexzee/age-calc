f365a4b2a4dbc4318bcedc0246fdafc07b4bbe3a
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-08-07 18:30:08 +0100

    Desktop styling done and active state modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './App.css'
 import submit from './assets/images/icon-arrow.svg'
 
 function App() {
+  const handleChange = ()=>{
+    console.log('Hello world');
+  }
 
   return (
     <>
+    <div className="body">
       <div className="App">
         <div className="inputs">
           <label htmlFor="day" className="day">
@@ -13,8 +17,10 @@ function App() {
               type="text"
               className="input"
               id="day"
-              value={"24"}
-            />
+              defaultValue={"24"}
+              onChange={handleChange}
+              />
+            <small className='error'>date error</small>
           </label>
           <label htmlFor="month" className="month">
             MONTH
@@ -22,8 +28,10 @@ function App() {
               type="text"
               className="input"
               id="month"
-              value={"09"}
+              defaultValue={"09"}
+              onChange={handleChange}
             />
+            <small className='error'>date error</small>
           </label>
           <label htmlFor="year" className="year">
             YEAR
@@ -31,8 +39,10 @@ function App() {
               type="text"
               className="input"
               id="year"
-              value={"1984"}
-            />
+              defaultValue={"1984"}
+              onChange={handleChange}
+              />
+            <small className='error'>date error</small>
           </label>
         </div>
         <div className="seperator">
@@ -45,10 +55,11 @@ function App() {
           <p><span className="days">{`26`}</span> days</p>
         </div>
       </div>
-      <div class="attribution">
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
         Coded by <a href="https://github.com/lexzee" target='_blank'>Lexzee</a>.
       </div>
+    </div>
     </>
   )
 }
