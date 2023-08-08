a0fcd684404f7e37213f77294b181d54b4c93388
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-08-08 14:01:15 +0100

    Added calculator function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
+import { useState } from 'react'
+import dayJs from 'dayjs'
 import './App.css'
 import submit from './assets/images/icon-arrow.svg'
+import dayjs from 'dayjs'
+// import { intervalToduration } from 'date-fns'
 
 function App() {
-  const handleChange = ()=>{
-    console.log('Hello world');
+  const [input, setInput] = useState({
+    day: "",
+    month: "",
+    year: ""
+  })
+
+  const [output, setOutput] = useState({
+    day: "--",
+    month: "--",
+    year: "--",
+  })
+
+  const handleChange = (e)=>{
+    setInput(state => {
+      return {
+        ...state,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  const getAgeDetails = (oldDate, newDate) => {
+    const years = newDate.diff(oldDate, 'year');
+    const months = newDate.diff(oldDate, 'months') - years * 12;
+    const days = newDate.diff(oldDate.add(years, 'year').add(months, 'month'), 'day')
+
+    return {
+      years,
+      months,
+      days,
+      allDays: newDate.diff(oldDate, 'day'),
+    };
+  };
+  const handleClick = ()=>{
+    const enteredDate = new Date(`${input.year}-${input.month}-${input.day}`);
+    // const enteredDate = new Date(Date.UTC(input.year,input.month,input.day));
+    const currentDate = new Date();
+
+    const oldDate=dayjs(enteredDate.toISOString());
+    const newDate=dayjs(currentDate.toISOString());
+
+    const date = (`${input.year}-${input.month}-${input.day}`);
+
+    console.log(`Date entered is ${enteredDate.toDateString()}`);
+    console.log(`Current date: ${currentDate}`);
+    // console.log(getAgeDetails(oldDate, newDate));
+
+    setOutput({
+      day: getAgeDetails(oldDate, newDate).days,
+      month: getAgeDetails(oldDate, newDate).months,
+      year: getAgeDetails(oldDate, newDate).years
+    })
+
   }
 
   return (
@@ -17,7 +72,9 @@ function App() {
               type="text"
               className="input"
               id="day"
-              defaultValue={"24"}
+              name='day'
+              // value={input.day}
+              placeholder='DD'
               onChange={handleChange}
               />
             <small className='error'>date error</small>
@@ -28,7 +85,9 @@ function App() {
               type="text"
               className="input"
               id="month"
-              defaultValue={"09"}
+              name='month'
+              // value={input.month}
+              placeholder='MM'
               onChange={handleChange}
             />
             <small className='error'>date error</small>
@@ -39,7 +98,9 @@ function App() {
               type="text"
               className="input"
               id="year"
-              defaultValue={"1984"}
+              name='year'
+              // value={input.year}
+              placeholder='YYYY'
               onChange={handleChange}
               />
             <small className='error'>date error</small>
@@ -47,12 +108,12 @@ function App() {
         </div>
         <div className="seperator">
           <hr />
-          <img src={submit} alt="" className="submit"/>
+          <img src={submit} alt="" className="submit" onClick={handleClick} />
         </div>
         <div className="output">
-          <p><span className="years">{`38`}</span> years</p>
-          <p><span className="months">{`3`}</span> months</p>
-          <p><span className="days">{`26`}</span> days</p>
+          <p><span className="years">{output.year}</span> years</p>
+          <p><span className="months">{output.month}</span> months</p>
+          <p><span className="days">{output.day}</span> days</p>
         </div>
       </div>
       <div className="attribution">
